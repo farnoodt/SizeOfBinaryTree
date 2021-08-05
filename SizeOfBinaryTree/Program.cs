31622a9c2a63d0dbc3f7a9fd2a81ccfc1c01f355
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-04 23:58:36 -0700

    Add project files.

---
+++ SizeOfBinaryTree/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SizeOfBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(20);
+            tree.left = new Node(15);
+            tree.left.left = new Node(10);
+            tree.left.right = new Node(17);
+            tree.right = new Node(30);
+
+            Size s = new Size();
+            Console.WriteLine(s.SizeOfTree(tree));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SizeOfBinaryTree/Size.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeOfBinaryTree
+{
+    class Size
+    {
+        public int SizeOfTree(Node root)
+        {
+            int right = 0;
+            int left = 0;
+            if (root == null)
+                return 0;
+            left = SizeOfTree(root.left);
+            right = SizeOfTree(root.right);
+
+            return left + right + 1;
+        }
+    }
+}
